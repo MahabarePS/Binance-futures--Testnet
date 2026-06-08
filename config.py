af0aba292e1e0b02941e753eb9a57c54e2331f53
MahabarePS <prasadmahabare1@gmail.com>
2026-06-09 00:48:26 +0530

    feat: added client wrapper

--- config.py
+++ config.py
@@ -5,3 +5,22 @@
  * copyright 2026
 """
 
+# bot/logging_config.py
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("BINANCE_API_KEY")
+API_SECRET = os.getenv("BINANCE_API_SECRET")
+
+import logging
+
+def setup_logger():
+
+    logging.basicConfig(
+        filename="logs/trading.log",
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(message)s"
+    )
